6084b0b6982e7bc801485c0e04abc41be0acdf8b
Anthony G <anthony.gorson@gmail.com>
2021-09-12 14:54:16 +0200

    removal of companies example

--- src/Controller/QuizzController.php
+++ src/Controller/QuizzController.php
@@ -13,22 +13,8 @@ class QuizzController extends AbstractController
      */
     public function index()
     {
-        $companies = [
-            'Apple' => '$1.16 trillion USD',
-            'Samsung' => '$298.68 billion USD',
-            'Microsoft' => '$1.10 trillion USD',
-            'Alphabet' => '$878.48 billion USD',
-            'Intel Corporation' => '$245.82 billion USD',
-            'IBM' => '$120.03 billion USD',
-            'Facebook' => '$552.39 billion USD',
-            'Hon Hai Precision' => '$38.72 billion USD',
-            'Tencent' => '$3.02 trillion USD',
-            'Oracle' => '$180.54 billion USD',
-        ];
-
         return $this->render('quizz/index.html.twig', [
             'controller_name' => 'QuizzController',
-            'companies' => $companies,
         ]);
     }
 }
